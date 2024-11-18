c2036cbca1ef60b30d3df1bdc6e9e755eda7a119
davidguillen2002 <davidanguillen@gmail.com>
2024-11-17 23:55:08 -0500

    Cambios Funcionales Finales

--- ServiceTrack/views.py
+++ ServiceTrack/views.py
@@ -1,41 +1,17 @@
 # ServiceTrack/views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
-from dashboard.views import dashboard_view
-
-User = get_user_model()
 
 @login_required
 def home_view(request):
     """Redirige al usuario a su vista principal según el rol."""
-    if not hasattr(request.user, 'rol') or not request.user.rol:
-        return render(request, 'error.html', {'message': 'No tienes un rol asignado.'})
-
     user_role = request.user.rol.nombre.lower()
 
     if user_role == "administrador":
-        return redirect('admin_dashboard')
+        return redirect('dashboard')
     elif user_role == "tecnico":
-        return redirect('tecnico_home')
+        return redirect('tecnico_dashboard')
     elif user_role == "cliente":
-        return redirect('cliente_home')
+        return redirect('cliente_dashboard')
     else:
         return render(request, 'error.html', {'message': 'Rol desconocido.'})
-
-
-@login_required
-def admin_dashboard_view(request):
-    """Vista del dashboard para el administrador."""
-    # Reutiliza la lógica de dashboard_view
-    return dashboard_view(request)
-
-@login_required
-def tecnico_home_view(request):
-    """Vista principal para técnicos."""
-    return render(request, 'service_track/tecnico_home.html')
-
-@login_required
-def cliente_home_view(request):
-    """Vista principal para clientes."""
-    return render(request, 'service_track/cliente_home.html')
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,11 @@
 # dashboard/urls.py
 from django.urls import path
-from .views import UsuarioListView, UsuarioCreateView, UsuarioUpdateView, UsuarioDeleteView, dashboard_view
+from .views import UsuarioListView, UsuarioCreateView, UsuarioUpdateView, UsuarioDeleteView, dashboard_view, tecnico_dashboard_view, cliente_dashboard_view
 
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),  # Ruta principal del dashboard
+    path('tecnico/', tecnico_dashboard_view, name='tecnico_dashboard'),
+    path('cliente/', cliente_dashboard_view, name='cliente_dashboard'),
     path('usuarios/', UsuarioListView.as_view(), name='usuario-list'),  # Lista de usuarios
     path('usuarios/nuevo/', UsuarioCreateView.as_view(), name='usuario-create'),  # Formulario para agregar usuario
     path('usuarios/editar/<int:pk>/', UsuarioUpdateView.as_view(), name='usuario-update'),  # Actualizar usuario
