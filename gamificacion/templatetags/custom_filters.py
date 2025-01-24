c265bafb54d93e60f4c9d5ac56f32cfcd7ce495a
davidguillen2002 <davidanguillen@gmail.com>
2025-01-23 22:55:21 -0500

    Render deploy completo

--- gamificacion/templatetags/custom_filters.py
+++ gamificacion/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+import os
 
 register = template.Library()
 
@@ -6,3 +7,10 @@ register = template.Library()
 def startswith(value, arg):
     """Verifica si una cadena comienza con el argumento proporcionado"""
     return str(value).startswith(arg)
+
+@register.filter
+def basename(value):
+    """
+    Extrae solo el nombre del archivo de una ruta.
+    """
+    return os.path.basename(value)
