ef689683d489536453331411ae429dd56359bb20
davidguillen2002 <davidanguillen@gmail.com>
2025-01-26 20:55:15 -0500

    Render últimos cambios

--- notificaciones/urls.py
+++ notificaciones/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.lista_notificaciones, name='lista_notificaciones'),
     path('<int:notificacion_id>/', views.detalle_notificacion, name='detalle_notificacion'),
     path('marcar_todas/', views.marcar_todas_como_leidas, name='marcar_todas_como_leidas'),
+    path('limpiar/', views.limpiar_notificaciones, name='limpiar_notificaciones'),  # Nueva ruta
 ]
--- notificaciones/views.py
+++ notificaciones/views.py
@@ -1,14 +1,14 @@
-# notificaciones/views.py
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from ServiceTrack.models import Notificacion
 
 @login_required
 def lista_notificaciones(request):
     """
-    Muestra la lista de notificaciones para el usuario actual, filtradas por rol.
+    Muestra la lista de notificaciones solo del usuario actual.
     """
-    notificaciones = Notificacion.obtener_notificaciones_por_rol(request.user)
+    notificaciones = Notificacion.objects.filter(usuario=request.user).order_by('-fecha_creacion')
     return render(request, 'notificaciones/lista_notificaciones.html', {'notificaciones': notificaciones})
 
 @login_required
@@ -28,4 +28,19 @@ def marcar_todas_como_leidas(request):
     Marca todas las notificaciones no leídas como leídas para el usuario actual.
     """
     Notificacion.objects.filter(usuario=request.user, leido=False).update(leido=True)
+    messages.success(request, "Todas tus notificaciones han sido marcadas como leídas.")
+    return redirect('lista_notificaciones')
+
+@login_required
+def limpiar_notificaciones(request):
+    """
+    Elimina todas las notificaciones del usuario actual y redirige a la lista.
+    """
+    try:
+        # Eliminar todas las notificaciones asociadas al usuario actual
+        Notificacion.objects.filter(usuario=request.user).delete()
+        messages.success(request, "Todas tus notificaciones han sido eliminadas exitosamente.")
+    except Exception as e:
+        messages.error(request, f"Error al intentar eliminar las notificaciones: {str(e)}")
+
     return redirect('lista_notificaciones')
